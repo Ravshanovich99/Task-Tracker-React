7b7b6dc78fd9607db06be4e976854dcc2847a1cb
Ravshanovich <niwonboyevrustam@gmail.com>
2022-07-08 23:10:39 +0500

    Updates

---
+++ precache-manifest.471c275f125285b0c5060ae69529a2b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f24292efc78000946968",
+    "url": "/Task-Tracker-React/static/css/main.95488e62.chunk.css"
+  },
+  {
+    "revision": "f24292efc78000946968",
+    "url": "/Task-Tracker-React/static/js/main.f24292ef.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Task-Tracker-React/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "edbb3e7f96477054ae06",
+    "url": "/Task-Tracker-React/static/js/2.edbb3e7f.chunk.js"
+  },
+  {
+    "revision": "957e40c43f5f4eb83b1a",
+    "url": "/Task-Tracker-React/static/js/runtime~main.957e40c4.js"
+  },
+  {
+    "revision": "90b1ecef9b2cdd7d73b9395dff668ccc",
+    "url": "/Task-Tracker-React/index.html"
+  }
+];
